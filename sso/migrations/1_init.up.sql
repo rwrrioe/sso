92cbb1cad97eff97361073f9458cec8fe1984176
rrwrrioe <damir100609@gmail.com>
2025-12-27 23:43:34 +0500

    add migrations

---
+++ sso/migrations/1_init.up.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id INT,
+    email TEXT NOT NULL UNIQUE,
+    pass_hash BYTEA NOT NULL,
+
+    CONSTRAINT pk_users PRIMARY KEY(id)
+);
+CREATE INDEX IF NOT EXISTS idx_email ON users(email)
+
+CREATE TABLE IF NOT EXISTS apps
+(
+    id INT,
+    name TEXT NOT NULL UNIQUE,
+    secret TEXT NOT NULL UNIQUE,
+
+    CONSTRAINT pk_apps PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS roles
+(
+    id INT,
+    role VARCHAR(100)
+
+    CONSTRAINT pk_roles PRIMARY KEY(role_id)
+)
+
+CREATE TABLE IF NOT EXISTS roles_users
+(
+    role_id INT,
+    user_id INT,
+
+    CONSTRAINT pk_roles_users PRIMARY KEY(role_id, user_id),
+    CONSTRAINT fk_roles_users_users FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_roles_users_roles FOREIGN KEY (role_id) REFERENCES roles(id)
+)
