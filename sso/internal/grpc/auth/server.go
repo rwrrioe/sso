ae64d7668c77e7c4ce2ec84613f45efa22f47d61
rrwrrioe <damir100609@gmail.com>
2025-12-27 23:42:03 +0500

    refactor server.go

--- sso/internal/grpc/auth/server.go
+++ sso/internal/grpc/auth/server.go
@@ -2,12 +2,17 @@ package auth
 
 import (
 	"context"
+	"errors"
 
 	ssov1 "github.com/GolangLessons/protos/gen/go/sso"
+	"github.com/rwrrioe/sso/internal/storage"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type serverAPI struct {
+	auth Auth
 	ssov1.UnimplementedAuthServer
 }
 
@@ -34,50 +39,50 @@ func (s *serverAPI) Login(
 	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
-	// if req.Email == "" {
-	// 	return nil, status.Error(codes.InvalidArgument, "email is required")
-	// }
+	if req.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
 
-	// if req.Password == "" {
-	// 	return nil, status.Error(codes.InvalidArgument, "password is required")
-	// }
+	if req.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
 
-	// if req.GetAppId() == 0 {
-	// 	return nil, status.Error(codes.InvalidArgument, "app_id is required")
-	// }
+	if req.GetAppId() == 0 {
+		return nil, status.Error(codes.InvalidArgument, "app_id is required")
+	}
 
-	// token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
-	// if err != nil {
-	// 	if errors.Is(err, auth.ErrInvlidCredentials) {
-	// 		return nil, status.Error(codes.InvalidArgument, "invalid email or password")
-	// 	}
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		if errors.Is(err, auth.ErrInvlidCredentials) {
+			return nil, status.Error(codes.InvalidArgument, "invalid email or password")
+		}
 
-	// 	return nil, status.Error(codes.Internal, "failed to login")
-	// }
+		return nil, status.Error(codes.Internal, "failed to login")
+	}
 
-	return nil, nil
+	return &ssov1.LoginResponse{Token: token}, nil
 }
 
 func (s *serverAPI) Register(
 	ctx context.Context,
 	req *ssov1.RegisterRequest,
 ) (*ssov1.RegisterResponse, error) {
-	// if req.Email == "" {
-	// 	return nil, status.Error(codes.InvalidArgument, "email is required")
-	// }
+	if req.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
 
-	// if req.Password == "" {
-	// 	return nil, status.Error(codes.InvalidArgument, "password is required")
-	// }
+	if req.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
 
-	// uid, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
-	// if err != nil {
-	// 	if errors.Is(err, storage.ErrUserExists) {
-	// 		return nil, status.Error(codes.AlreadyExists, "user already exists")
-	// 	}
+	uid, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			return nil, status.Error(codes.AlreadyExists, "user already exists")
+		}
 
-	// 	return nil, status.Error(codes.Internal, "failed to register user")
-	// }
+		return nil, status.Error(codes.Internal, "failed to register user")
+	}
 
-	return nil, nil
+	return &ssov1.RegisterResponse{UserId: uid}, nil
 }
