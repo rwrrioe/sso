07d27b2c13a7b08bc19a974ae148d7c7ed7c037d
rrwrrioe <damir100609@gmail.com>
2026-03-21 19:30:12 +0200

    refactor: move auth ports to a distinct file

---
+++ internal/usecase/auth/ports.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/rwrrioe/sso/internal/domain/models"
+)
+
+type UserSaver interface {
+	SaveUser(
+		ctx context.Context,
+		email string,
+		passHash []byte,
+	) (uid uuid.UUID, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (*models.User, error)
+	IsAdmin(ctx context.Context, userID uuid.UUID) (bool, error)
+
+	ResetPassword(ctx context.Context, email string) error
+	SetNewPassword(ctx context.Context, email string, passHash []byte) error
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (*models.App, error)
+}
+
+type CodeProvider interface {
+	//TODO polish reset code provider
+	SaveCode(ctx context.Context, code int) (int, error)
+	ResetCode(ctx context.Context, code int) (models.ResetCode, error)
+	MarkUsed(ctx context.Context, code int) error
+}
+
+type MailProvider interface {
+	SendCode(ctx context.Context, email, code string) error
+}
