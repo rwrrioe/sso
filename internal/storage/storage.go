554029dbadc1843f5b4c9314ef67216da09c7d30
rrwrrioe <damir100609@gmail.com>
2026-03-21 19:31:43 +0200

    refactor: repository returns domain errors only

--- internal/storage/storage.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-import "errors"
-
-var (
-	ErrUserExists   = errors.New("user already exists")
-	ErrUserNotFound = errors.New("user not found")
-	ErrAppNotFound  = errors.New("app not found")
-)
