b4fed954615bc417c06ac425fe48d980f3aaa5e0
rrwrrioe <damir100609@gmail.com>
2026-03-28 11:41:29 +0200

    fix:fix foreign key bugs

--- migrations/2_refresh_token.up.sql
+++ migrations/2_refresh_token.up.sql
@@ -7,12 +7,13 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     user_id UUID,
     email VARCHAR(320),
     app_id INT,
+    used BOOLEAN,
     expires_at TIMESTAMP,
     created_at TIMESTAMP DEFAULT NOW(),
 
     CONSTRAINT pk_refresh_tokens PRIMARY KEY(token),
-    CONSTRAINT fk_refresh_tokens_users FOREIGN KEY REFERENCES users(id),
-    CONSTRAINT fk_refresh_tokens_apps FOREIGN KEY REFERENCES apps(id)
+    CONSTRAINT fk_refresh_tokens_users FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_refresh_tokens_apps FOREIGN KEY (app_id) REFERENCES apps(id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_refresh_token ON refresh_tokens(token);
